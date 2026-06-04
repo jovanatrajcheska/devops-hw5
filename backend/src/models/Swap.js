5b28621f4ef1aa8d46a465af83d5350e478194f8
jovanatrajcheska <jovanatrajceska88@gmail.com>
2026-06-04 11:55:00 +0200

    test flow

--- backend/src/models/Swap.js
+++ backend/src/models/Swap.js
@@ -8,5 +8,5 @@ const swapSchema = new mongoose.Schema({
   message: String,
   status: { type: String, enum: ['pending', 'accepted', 'declined', 'picked_up'], default: 'pending' }
 }, { timestamps: true });
-
+// change something
 export default mongoose.model('Swap', swapSchema);
